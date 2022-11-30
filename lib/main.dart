07108e7e9427efe4ef144cabd6b1b5e845e0a2df
nguyendoan18 <nguyendoan403@gmail.com>
2022-11-30 08:42:37 +0700

    Quan ly trang thai voi provider - ProductsManager

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'ui/products/products_overview_screen.dart';
 import 'ui/products/user_products_screen.dart';
 import 'ui/cart/cart_screen.dart';
 import 'ui/orders/orders_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,55 +17,74 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'My Shop',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        fontFamily: 'Lato',
-        colorScheme: ColorScheme.fromSwatch(
-          primarySwatch: Colors.purple,
-        ).copyWith(
-          secondary: Colors.deepOrange,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => ProductsManager(),
         ),
+      ],
+      child: MaterialApp(
+        title: 'My Shop',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          fontFamily: 'Lato',
+          colorScheme: ColorScheme.fromSwatch(
+            primarySwatch: Colors.purple,
+          ).copyWith(
+            secondary: Colors.deepOrange,
+          ),
+        ),
+        // home: Container(
+        //   color: Colors.green,
+        // ),
+        // home: SafeArea(
+        //     child: ProductDetailScreen(
+        //   ProductManager().items[0],
+        // )),
+        // home: const SafeArea(
+        //   child: ProductsOverviewScreen(),
+        // ),
+        // home: const SafeArea(
+        //   child: UserProductsScreen(),
+        // ),
+        // home: const SafeArea(
+        //   child: CartScreen(),
+        // ),
+        // home: const SafeArea(
+        //   child: OrdersScreen(),
+        // ),
+        home: const ProductsOverviewScreen(),
+        routes: {
+          CartScreen.routeName: (ctx) => const CartScreen(),
+          OrdersScreen.routeName: (ctx) => const OrdersScreen(),
+          UserProductsScreen.routeName: (ctx) => const UserProductsScreen(),
+        },
+        onGenerateRoute: (settings) {
+          // if (settings.name == ProductDetailScreen.routeName) {
+          //   final productId = settings.arguments as String;
+          //   return MaterialPageRoute(
+          //     builder: (ctx) {
+          //       return ProductDetailScreen(
+          //         ctx.read<ProductsManager>().findById(productId),
+          //       );
+          //     },
+          //   );
+          // }
+          if (settings.name == ProductDetailScreen.routeName) {
+            final productId = settings.arguments as String;
+            return MaterialPageRoute(
+              builder: (ctx) {
+                return ProductDetailScreen(
+                  ctx.read<ProductsManager>().findById(productId)!,
+                  //ProductsManager().findById(productId)!,
+                );
+              },
+            );
+          }
+
+          return null;
+        },
       ),
-      // home: Container(
-      //   color: Colors.green,
-      // ),
-      // home: SafeArea(
-      //     child: ProductDetailScreen(
-      //   ProductManager().items[0],
-      // )),
-      // home: const SafeArea(
-      //   child: ProductsOverviewScreen(),
-      // ),
-      // home: const SafeArea(
-      //   child: UserProductsScreen(),
-      // ),
-      // home: const SafeArea(
-      //   child: CartScreen(),
-      // ),
-      // home: const SafeArea(
-      //   child: OrdersScreen(),
-      // ),
-      home: const ProductsOverviewScreen(),
-      routes: {
-        CartScreen.routeName: (ctx) => const CartScreen(),
-        OrdersScreen.routeName: (ctx) => const OrdersScreen(),
-        UserProductsScreen.routeName: (ctx) => const UserProductsScreen(),
-      },
-      onGenerateRoute: (settings) {
-        if (settings.name == ProductDetailScreen.routeName) {
-          final productId = settings.arguments as String;
-          return MaterialPageRoute(
-            builder: (ctx) {
-              return ProductDetailScreen(
-                ProductsManager().findById(productId)!,
-              );
-            },
-          );
-        }
-        return null;
-      },
     );
   }
 }
--- lib/ui/products/user_product_list_tile.dart
+++ lib/ui/products/user_product_list_tile.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:lab2_myshop/ui/products/products_manager.dart';
+import 'package:provider/provider.dart';
 import '../../models/product.dart';
 
 class UserProductListTile extends StatelessWidget {
@@ -30,21 +32,51 @@ class UserProductListTile extends StatelessWidget {
 
   Widget buildDeleteButton(BuildContext context) {
     return IconButton(
+      // icon: const Icon(Icons.delete),
+      // onPressed: () {
+      //   print('Delete a product');
+      // },
+      // color: Theme.of(context).errorColor,
       icon: const Icon(Icons.delete),
       onPressed: () {
-        print('Delete a product');
+        context.read<ProductsManager>().deleteProduct(product.id!);
+        ScaffoldMessenger.of(context)
+          ..hideCurrentSnackBar()
+          ..showSnackBar(
+            const SnackBar(
+              content: Text(
+                'Product deleted',
+                textAlign: TextAlign.center,
+              ),
+            ),
+          );
       },
       color: Theme.of(context).errorColor,
     );
   }
 
   Widget buildEditButton(BuildContext context) {
-    return IconButton(
-      icon: const Icon(Icons.edit),
-      onPressed: () {
-        print('Go to edit product screen');
+    // return IconButton(
+    //   icon: const Icon(Icons.edit),
+    //   onPressed: () {
+    //     print('Go to edit product screen');
+    //   },
+    //   color: Theme.of(context).primaryColor,
+    // );
+    return Consumer<ProductsManager>(
+      builder: (ctx, productsManager, child) {
+        return ListView.builder(
+          itemCount: productsManager.itemCount,
+          itemBuilder: (ctx, i) => Column(
+            children: [
+              UserProductListTile(
+                productsManager.items[i],
+              ),
+              const Divider(),
+            ],
+          ),
+        );
       },
-      color: Theme.of(context).primaryColor,
     );
   }
 }
